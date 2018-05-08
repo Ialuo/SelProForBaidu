56979d411dd96edc9c82f1b1cd4ea8ae165daf35
Ialuo <746090675@qq.com>
2018-05-08 14:19:44 +0800

    `

--- src/main/java/Utils/DriverHelper.java
+++ src/main/java/Utils/DriverHelper.java
@@ -3,15 +3,18 @@ package Utils;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverHelper {
 	protected static WebDriver driver;
 	protected static Properties p;
-	protected static String config="xxx";
+	protected static String config="D:\\Program Files (x86)\\SmartGit\\bin\\github.com\\zhiminliao\\SelProForBaidu\\config.properties";
 	protected static String chromeDriver;
 	protected static int iWaitTime;
+	protected static Logger logger = LogManager.getLogger(DriverHelper.class);
 	
 	
 	
@@ -28,11 +31,17 @@ public class DriverHelper {
 				chromeDriver = p.getProperty("chromedriver");
 				iWaitTime = Integer.parseInt(p.getProperty("WaitTime"));	
 			}
-			System.setpro'
-			
-			
+			System.setProperty("webdriver.chrome.driver",chromeDriver);
+			driver= new ChromeDriver();
+			logger.info("Start WebDriver");
+			driver.manage().window().maximize();				  
 		}
+		return driver;
 		
+	}
+	public static void stop(){
+		driver.quit();
+		logger.info("Stop WebDriver");
 	}
 	
 
---
+++ src/test/java/BaiduTestCase/homePage.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package BaiduTestCase;
+public class homePage{
+	
+}
